4fc9eb5a5e4447ae641d6a234abb2ce7f218cbcf
Rioto <xxxoojbjb@gmail.com>
2025-03-26 16:40:15 +0900

    404ページには404の内容だけを表示させるようにした。

--- 404.php
+++ 404.php
@@ -22,59 +22,9 @@ get_header();
                     </a>
                     
                     <div class="error-search">
-                        <p>または、キーワードで検索してみてください：</p>
+
+
                         <?php get_search_form(); ?>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="suggested-content">
-                <h2>人気のイベントカテゴリー</h2>
-                <div class="categories-grid">
-                    <?php
-                    $categories = get_terms(array(
-                        'taxonomy' => 'event_category',
-                        'hide_empty' => true,
-                        'number' => 4
-                    ));
-
-                    if (!empty($categories) && !is_wp_error($categories)) :
-                        foreach ($categories as $category) :
-                            ?>
-                            <a href="<?php echo esc_url(get_term_link($category)); ?>" class="category-card">
-                                <div class="category-icon">
-                                    <i class="fas fa-<?php echo esc_attr(get_category_icon($category->slug)); ?>"></i>
-                                </div>
-                                <h3><?php echo esc_html($category->name); ?></h3>
-                                <p><?php echo esc_html($category->description); ?></p>
-                            </a>
-                            <?php
-                        endforeach;
-                    endif;
-                    ?>
-                </div>
-                
-                <div class="recent-events">
-                    <h2>最新のイベント</h2>
-                    <div class="events-grid">
-                        <?php
-                        $args = array(
-                            'post_type' => 'event',
-                            'posts_per_page' => 3,
-                            'orderby' => 'date',
-                            'order' => 'DESC'
-                        );
-                        $recent_events = new WP_Query($args);
-
-                        if ($recent_events->have_posts()) :
-                            while ($recent_events->have_posts()) : $recent_events->the_post();
-                                get_template_part('template-parts/content', 'card');
-                            endwhile;
-                            wp_reset_postdata();
-                        else :
-                            echo '<p class="no-events">現在開催予定のイベントはありません。</p>';
-                        endif;
-                        ?>
                     </div>
                 </div>
             </div>
