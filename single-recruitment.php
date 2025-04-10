835067748eb384f2c0756e29c3bdf2ec0a21b1e2
Rioto <xxxoojbjb@gmail.com>
2025-04-10 15:39:18 +0900

    タイトルのフォントサイズ変更

--- single-recruitment.php
+++ single-recruitment.php
@@ -21,14 +21,6 @@ while (have_posts()) :
 </section>
 
 <article class="container mx-auto px-4 py-8" style="position: relative; z-index: 10; margin:100px 70px;">
-    <!-- 戻るボタン -->
-    <a href="<?php echo get_post_type_archive_link('recruitment'); ?>" class="inline-flex items-center text-gray-600 mb-6 hover:text-gray-900 transition-colors">
-        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
-        </svg>
-        募集一覧に戻る
-    </a>
-
     <!-- タイトル -->
     <h1 class="recruitment-title font-bold mb-8"><?php the_title(); ?></h1>
 
