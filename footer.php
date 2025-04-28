57aaaf21ac3698ee505c03fdd4adc217b634b38a
Rioto <xxxoojbjb@gmail.com>
2025-04-28 14:38:40 +0900

    フッターを英訳にしました。

--- footer.php
+++ footer.php
@@ -4,23 +4,23 @@
         <div class="footer-container">
             <div class="footer-widgets">
                 <div class="footer-widget">
-                    <h3>サイトについて</h3>
+                    <h3>About Site</h3>
                     <ul>
-                        <li><a href="<?php echo esc_url(home_url('/about')); ?>">AIMappingとは</a></li>
-                        <li><a href="<?php echo esc_url(home_url('/guide')); ?>">使い方ガイド</a></li>
-                        <li><a href="<?php echo esc_url(home_url('/faq')); ?>">よくある質問</a></li>
+                        <li><a href="<?php echo esc_url(home_url('/about')); ?>">About AIMapping</a></li>
+                        <li><a href="<?php echo esc_url(home_url('/guide')); ?>">User Guide</a></li>
+                        <li><a href="<?php echo esc_url(home_url('/faq')); ?>">FAQ</a></li>
                     </ul>
                 </div>
                 <div class="footer-widget">
-                    <h3>サポート</h3>
+                    <h3>Support</h3>
                     <ul>
-                        <li><a href="<?php echo esc_url(home_url('/contact')); ?>">お問い合わせ</a></li>
-                        <li><a href="<?php echo esc_url(home_url('/terms')); ?>">利用規約</a></li>
-                        <li><a href="<?php echo esc_url(home_url('/privacy')); ?>">プライバシーポリシー</a></li>
+                        <li><a href="<?php echo esc_url(home_url('/contact')); ?>">Contact</a></li>
+                        <li><a href="<?php echo esc_url(home_url('/terms')); ?>">Terms of Service</a></li>
+                        <li><a href="<?php echo esc_url(home_url('/privacy')); ?>">Privacy Policy</a></li>
                     </ul>
                 </div>
                 <div class="footer-widget">
-                    <h3>フォローする</h3>
+                    <h3>Follow Us</h3>
                     <div class="social-links">
                         <a href="#" class="social-link twitter"><i class="fab fa-twitter"></i></a>
                         <a href="#" class="social-link facebook"><i class="fab fa-facebook"></i></a>
@@ -29,11 +29,11 @@
                 </div>
             </div>
             <div class="footer-bottom">
-                <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
+                <p>&copy; 2025 AI_Mapping. All rights reserved.</p>
             </div>
         </div>
     </footer>
 
     <?php wp_footer(); ?>
 </body>
-</html> 
+</html>
