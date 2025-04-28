9b729b93e8954fa9d79d5616ee908683b7def036
Rioto <xxxoojbjb@gmail.com>
2025-04-28 15:27:13 +0900

    フッターのレイアウト修正

--- footer.php
+++ footer.php
@@ -2,29 +2,31 @@
 
     <footer class="site-footer">
         <div class="footer-container">
-            <div class="footer-widgets">
-                <div class="footer-widget">
-                    <h3>About Site</h3>
-                    <ul>
-                        <li><a href="<?php echo esc_url(home_url('/about')); ?>">About AIMapping</a></li>
-                        <li><a href="<?php echo esc_url(home_url('/guide')); ?>">User Guide</a></li>
-                        <li><a href="<?php echo esc_url(home_url('/faq')); ?>">FAQ</a></li>
-                    </ul>
-                </div>
-                <div class="footer-widget">
-                    <h3>Support</h3>
-                    <ul>
-                        <li><a href="<?php echo esc_url(home_url('/contact')); ?>">Contact</a></li>
-                        <li><a href="<?php echo esc_url(home_url('/terms')); ?>">Terms of Service</a></li>
-                        <li><a href="<?php echo esc_url(home_url('/privacy')); ?>">Privacy Policy</a></li>
-                    </ul>
-                </div>
-                <div class="footer-widget">
-                    <h3>Follow Us</h3>
-                    <div class="social-links">
-                        <a href="#" class="social-link twitter"><i class="fab fa-twitter"></i></a>
-                        <a href="#" class="social-link facebook"><i class="fab fa-facebook"></i></a>
-                        <a href="#" class="social-link instagram"><i class="fab fa-instagram"></i></a>
+            <div class="footer-widgets-wrapper">
+                <div class="footer-widgets">
+                    <div class="footer-widget">
+                        <h3>About Site</h3>
+                        <ul>
+                            <li><a href="<?php echo esc_url(home_url('/about')); ?>">About AIMapping</a></li>
+                            <li><a href="<?php echo esc_url(home_url('/guide')); ?>">User Guide</a></li>
+                            <li><a href="<?php echo esc_url(home_url('/faq')); ?>">FAQ</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-widget">
+                        <h3>Support</h3>
+                        <ul>
+                            <li><a href="<?php echo esc_url(home_url('/contact')); ?>">Contact</a></li>
+                            <li><a href="<?php echo esc_url(home_url('/terms')); ?>">Terms of Service</a></li>
+                            <li><a href="<?php echo esc_url(home_url('/privacy')); ?>">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-widget">
+                        <h3>Follow Us</h3>
+                        <div class="social-links">
+                            <a href="#" class="social-link twitter"><i class="fab fa-twitter"></i></a>
+                            <a href="#" class="social-link facebook"><i class="fab fa-facebook"></i></a>
+                            <a href="#" class="social-link instagram"><i class="fab fa-instagram"></i></a>
+                        </div>
                     </div>
                 </div>
             </div>
