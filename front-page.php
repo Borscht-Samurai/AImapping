69ef41c84895004ccc1294cec1dd7088a2893c37
Rioto <xxxoojbjb@gmail.com>
2025-05-22 16:53:09 +0900

    front fix

--- front-page.php
+++ front-page.php
@@ -17,9 +17,9 @@
     <section class="new-box-section">
         <div class="new-box-top">
             <div class="left-box-top">
-                <h2 style="width: 277.19px; height: 76px; font-family: 'Montserrat'; font-style: normal; font-weight: 500; font-size: 29.5667px; line-height: 130%; letter-spacing: -0.147834px; color: #535353; flex: none; order: 0; align-self: stretch; flex-grow: 0;">AIクリエイターの<br>出会いの場を探す</h2>
-                <p style="width: 277.19px; height: 120px; font-family: 'Montserrat'; font-style: normal; font-weight: 400; font-size: 14.7834px; line-height: 160%; display: flex; align-items: center; color: #535353; flex: none; order: 1; align-self: stretch; flex-grow: 0;">
-                    同じ地域のAIクリエイターと直接会って話せる。オンラインでは得られない、その場の空気感や熱量が新しいインスピレーションを生み出します。
+                <h2 class="new-box-title">AIクリエイターの<br>出会いの場を探す</h2>
+                <p class="new-box-description">
+                    同じ地域のAIクリエイターと直接会って<br>話せる。オンラインでは得られない、そ<br>の場の空気感や熱量が新しいインスピ<br>レーションを生み出します。
                 </p>
                 <a href="#" class="start-free-button">無料ではじめる</a>
                 <div class="picnic-container">
@@ -45,8 +45,8 @@
         <img src="<?php echo esc_url(get_template_directory_uri() . '/images/frontpage-3.png'); ?>" alt="フロントページ画像3" class="frontpage-image frontpage-3-image">
     </div>
     <!-- frontpage-4画像 中央揃え配置 -->
-    <div class="frontpage-image-container frontpage-3-container">
-        <img src="<?php echo esc_url(get_template_directory_uri() . '/images/frontpage-4.png'); ?>" alt="フロントページ画像4" class="frontpage-image frontpage-3-image">
+    <div class="frontpage-image-container frontpage-4-container">
+        <img src="<?php echo esc_url(get_template_directory_uri() . '/images/frontpage-4.png'); ?>" alt="フロントページ画像4" class="frontpage-image frontpage-4-image">
     </div>
 
 </main>
